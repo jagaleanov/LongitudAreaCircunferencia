87fdcf10748525379022b0c08b0eaaaf2f9f1a31
jagaleanov <54491111+jagaleanov@users.noreply.github.com>
2020-07-20 01:23:41 -0500

    Add files via upload

---
+++ long_y_area_de_circunferencia.py
@@ -0,0 +1,7 @@
+
+# Escribir un programa que calcule la longitud y el área de una circunferencia, 
+# capturando el valor del radio
+
+radio = int(input("radio del circulo: "))
+print("El máximo de la lista es ",(2 * 3.141592653589793 * radio)," y su area es ",
+((2 * 3.141592653589793 * radio * radio) / 2))
